08354f81d6d19aa676b273a48f012e81f3e153f0
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 16:44:26 +0500

    original code

--- shortest/script.js
+++ shortest/script.js
@@ -1,3 +1,23 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 6, 9];
 var target = 6;
 var foundIndex = -1;
+// arr.find((num, ))
+
+var arr = [1, 2, 5, 6, 7, 8, 9, 6, 3, 6];
+
+var foundElem = arr.find(findElem);
+var foundIndex = -1;
+function findElem(element, index, array) {
+  if (element != 6) {
+    return false;
+  }
+
+  if (element === 6 && array.indexOf(6, index + 1) && foundIndex === -1) {
+    foundIndex = array.indexOf(6, index + 1);
+    return false;
+  }
+  if (element === 6 && foundIndex !== -1) {
+    return true;
+  }
+  return false;
+}
