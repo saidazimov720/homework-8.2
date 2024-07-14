b4208b51bbf80f3f78ceb92fca69256cd4b99351
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 13:40:42 +0500

    correction

--- shortest/script.js
+++ shortest/script.js
@@ -7,7 +7,7 @@ if (cmd == "a-z") {
     var form1 = arr.sort((a, b) => a - b);
     alert(form1);
 } else if (cmd == "z-a") {
-    var form2 = arr.sort((a, b) => b - a);
+    var form2 = arr.sort().reverse();
     alert(form2);
 } else if (cmd == "0-9") {
     var form3 = arr.sort((a, b) => a - b);
