d0c697a1ad9a8e267068fd523ee3acbcc63bee25
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 16:19:43 +0500

    add the new features

--- sorter/script.js
+++ sorter/script.js
@@ -27,16 +27,27 @@ function specialsortreverse(a, b) {
     }
 }
 
-if (cmd == "a-z") {
+if (cmd == "a-z:0-9") {
     var form1 = arr.sort(specialsort);
     alert(form1.join(', '));
-} else if (cmd == "z-a") {
-    var form2 = arr.sort(specialsortreverse); 
+} else if (cmd == "z-a:9-0") {
+    var form2 = arr.sort(specialsortreverse);
     alert(form2.join(', '));
-} else if (cmd == "0-9") {
+} else if (cmd == "a-z") {
+    var charsOnly = arr.filter(item => !isNaN(item));
+    var form3 = charsOnly.sort((a, b) => a - b);
+    alert(form3);
+}
+else if (cmd == "z-a") {
+    var charsOnly = arr.filter(item => !isNaN(item));
+    var form3 = charsOnly.sort((a, b) => b - a);
+    alert(form3);
+}
+else if (cmd == "0-9") {
     var numbersonly = arr.filter(item => !isNaN(item));
-    var form3 = numbersonly.sort((a, b) => Number(a) - Number(b));
-    alert(form3.join(', '));
+    var form4 = numbersonly.sort((a, b) => Number(a) - Number(b));
+    // var sortedArray = arr.map(item => (!isNaN(item) ? numbersonly.shift() : item));
+    alert(form4.join(', '));
 } else if (cmd == "9-0") {
     var numbersonly = arr.filter(item => !isNaN(item));
     var form4 = numbersonly.sort((a, b) => Number(b) - Number(a));
