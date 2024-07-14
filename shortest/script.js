6c2a90f7533434bff106ad98be04dbef96643299
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 13:54:20 +0500

    second test

--- shortest/script.js
+++ shortest/script.js
@@ -3,7 +3,7 @@ var cmd = prompt("Enter the sort format");
 var arr = str.split(',');
 
 if (cmd == "a-z") {
-    var form1 = arr.sort();
+    var form1 = arr.sort((a,b) => a - b);
     alert(form1);
 } else if (cmd == "z-a") {
     var form2 = arr.sort().reverse();
