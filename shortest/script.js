4790af3fa7b0c52530a86a433f73e3308cfe3e97
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 16:46:39 +0500

    create the logic based on original

--- shortest/script.js
+++ shortest/script.js
@@ -2,7 +2,12 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8, 6, 9];
 var target = 6;
 var foundIndex = -1;
 
-
+arr.find((num, index) => {
+    if (num === target) {
+        foundIndex = index;
+    }
+    
+})
 var arr = [1, 2, 5, 6, 7, 8, 9, 6, 3, 6];
 
 var foundElem = arr.find(findElem);
