0f414c34fd941830f66b26062ae04d317af06db7
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 14:19:31 +0500

    function customing sort

--- shortest/script.js
+++ shortest/script.js
@@ -2,6 +2,10 @@ var str = prompt("Enter the array for sorting:");
 var cmd = prompt("Enter the sort format");
 var arr = str.split(',');
 
+function sort(a,b) {
+    
+}
+
 if (cmd == "a-z") {
     var form1 = arr.sort();
     alert(form1.join(', '));
