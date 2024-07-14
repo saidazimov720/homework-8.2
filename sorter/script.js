864fa5e1f38f16f8788527acad3f433c654b6b86
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 16:36:24 +0500

    fixing some bugs

--- sorter/script.js
+++ sorter/script.js
@@ -38,17 +38,16 @@ if (cmd == "a-z:0-9") {
 } else if (cmd == "a-z") {
     var charsOnly = arr.filter(item => !isNaN(item));
     var form3 = charsOnly.sort();
-    alert(form3);
+    alert(form3.join(', '));
 }
 else if (cmd == "z-a") {
     var charsOnly = arr.filter(item => !isNaN(item));
     var form3 = charsOnly.sort().reverse();
-    alert(form3);
+    alert(form3.join(', '));
 }
 else if (cmd == "0-9") {
     var numbersonly = arr.filter(item => !isNaN(item));
     var form4 = numbersonly.sort((a, b) => Number(a) - Number(b));
-    // var sortedArray = arr.map(item => (!isNaN(item) ? numbersonly.shift() : item));
     alert(form4.join(', '));
 } else if (cmd == "9-0") {
     var numbersonly = arr.filter(item => !isNaN(item));
