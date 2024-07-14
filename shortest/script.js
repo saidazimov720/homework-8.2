2765b8c245bbdf564ccb1ccf2f95c2e3835a77f4
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 14:07:12 +0500

    add the join method to the alert

--- shortest/script.js
+++ shortest/script.js
@@ -3,17 +3,18 @@ var cmd = prompt("Enter the sort format");
 var arr = str.split(',');
 
 if (cmd == "a-z") {
-    var form1 = arr.sort((a,b) => a - b);
-    alert(form1);
+    var form1 = arr.sort();
+    alert(form1.join(', '));
 } else if (cmd == "z-a") {
     var form2 = arr.sort().reverse();
-    alert(form2);
+    var strform2 = form2.toString();
+    alert(strform2.join(', '));
 } else if (cmd == "0-9") {
     var form3 = arr.sort((a, b) => Number(a) - Number(b));
-    alert(form3);
+    alert(form3.join(', '));
 } else if (cmd == "9-0") {
     var form4 = arr.sort((a, b) => Number(b) - Number(a));
-    alert(form4);
+    alert(form4.join(', '));
 } else {
     alert("invalid value");
 }
