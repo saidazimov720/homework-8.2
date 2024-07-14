10665887b02cfc60ce990b651635e99ccfab5924
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 13:35:17 +0500

    z-a format

--- shortest/script.js
+++ shortest/script.js
@@ -3,8 +3,10 @@ var cmd = prompt("Enter the sort format");
 var arr = str.split(',');
 
 
-if (cmd = "a-z" ) {
-    var form1 = arr.sort();
-} else {
-    
+if (cmd == "a-z") {
+    var form1 = arr.sort((a, b) => a - b);
+    alert(form1);
+} else if(cmd =="z-a" ) {
+    var form2 = arr.sort((a,b) => b-a);
+    alert(form2);
 }
