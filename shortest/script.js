9f8c7708f033066e91ea4fbedfff19ad7cf0ec16
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-14 13:31:33 +0500

    if else for sort

--- shortest/script.js
+++ shortest/script.js
@@ -2,6 +2,7 @@ var str = prompt("Enter the array for sorting:");
 var cmd = prompt("Enter the sort format");
 var arr = str.split(',');
 
+
 if (cmd = "a-z" ) {
     var form1 = arr.sort();
 } else {
